be56ca3982e61da0948cb1b0a7cdac490c8e83cb
kimyou1102 <yoosk5485@naver.com>
2024-11-07 19:23:46 +0900

    test: 프로모션 정보 클래스 테스트 작성

---
+++ __tests__/PromotionTest.js
@@ -0,0 +1,120 @@
+import PromotionInfo from '../src/model/PromotionInfo';
+
+describe('프로모션 정보 클래스 테스트', () => {
+  test('프로모션 정보를 가질 수 있다.', () => {
+    const PROMOTIONS = [
+      {
+        name: '탄산2+1',
+        buy: 2,
+        get: 1,
+        start_date: '2024-01-01',
+        end_date: '2024-12-31',
+      },
+      {
+        name: 'MD추천상품',
+        buy: 1,
+        get: 1,
+        start_date: '2024-01-01',
+        end_date: '2024-12-31',
+      },
+    ];
+
+    const promotionInfo = new PromotionInfo(PROMOTIONS);
+
+    expect(promotionInfo.getPromotionInfo()).toEqual(PROMOTIONS);
+  });
+
+  const BEFORE_DATE = '2023-12-31';
+  const AFTER_DATE = '2025-01-01';
+
+  test.each([
+    ['MD추천상품', BEFORE_DATE],
+    ['MD추천상품', AFTER_DATE],
+  ])(
+    '오늘 날짜가 프로모션 기간을 벗어났다면 적용할 수 없다.',
+    (PROMOTION, NOW_DATE) => {
+      const PROMOTIONS = [
+        {
+          name: 'MD추천상품',
+          buy: 1,
+          get: 1,
+          start_date: '2024-01-01',
+          end_date: '2024-12-31',
+        },
+      ];
+
+      const promotionInfo = new PromotionInfo(PROMOTIONS);
+      promotionInfo.isIncludePromotionDate(PROMOTION, NOW_DATE);
+
+      expect(promotionInfo.isIncludePromotionDate(PROMOTION, NOW_DATE)).toBe(
+        false,
+      );
+    },
+  );
+
+  const START_DATE = '2024-01-01';
+  const END_DATE = '2024-12-31';
+
+  test.each([
+    ['MD추천상품', START_DATE],
+    ['MD추천상품', END_DATE],
+  ])(
+    '오늘 날짜가 프로모션 기간 내에 포함된다면 할인을 적용할 수 있다.',
+    (PROMOTION, NOW_DATE) => {
+      const PROMOTIONS = [
+        {
+          name: '탄산2+1',
+          buy: 2,
+          get: 1,
+          start_date: '2024-01-01',
+          end_date: '2024-12-31',
+        },
+        {
+          name: 'MD추천상품',
+          buy: 1,
+          get: 1,
+          start_date: '2024-01-01',
+          end_date: '2024-12-31',
+        },
+      ];
+
+      const promotionInfo = new PromotionInfo(PROMOTIONS);
+      promotionInfo.isIncludePromotionDate(PROMOTION, NOW_DATE);
+
+      expect(promotionInfo.isIncludePromotionDate(PROMOTION, NOW_DATE)).toBe(
+        true,
+      );
+    },
+  );
+
+  test.each([
+    ['탄산2+1', 1, 1],
+    ['탄산2+1', 2, 0],
+    ['탄산2+1', 3, 1],
+    ['MD추천상품', 1, 2],
+  ])(
+    '프로모션 적용 가능한 상품인 경우 더 가져와야할 개수를 반환한다.',
+    (PROMOTION, QUANTITY, COUNT) => {
+      const PROMOTIONS = [
+        {
+          name: '탄산2+1',
+          buy: 2,
+          get: 1,
+          start_date: '2024-01-01',
+          end_date: '2024-12-31',
+        },
+        {
+          name: 'MD추천상품',
+          buy: 3,
+          get: 1,
+          start_date: '2024-01-01',
+          end_date: '2024-12-31',
+        },
+      ];
+
+      const promotionInfo = new PromotionInfo(PROMOTIONS);
+
+      expect(promotionInfo.insufficientCount(PROMOTION, QUANTITY)).toBe(COUNT);
+    },
+  );
+});
