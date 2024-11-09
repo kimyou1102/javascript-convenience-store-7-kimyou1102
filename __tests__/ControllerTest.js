262fdb2b7aa6d54363a9beade95b6d10f9578301
kimyou1102 <yoosk5485@naver.com>
2024-11-10 04:20:29 +0900

    test: 컨트롤러 클래스 테스트 수정
    - 잘못된 계산 로직에 맞춰 작성된 테스트 결과값을 올바른 로직에 맞게 수정

--- __tests__/ControllerTest.js
+++ __tests__/ControllerTest.js
@@ -59,18 +59,20 @@ describe('컨트롤러 테스트', () => {
   test('구입할 상품 중 프로모션이 가능한 상품 목록을 구할 수 있다.', () => {
     const result = [{ name: '사이다', quantity: 1, promotion: '탄산2+1' }];
     const productToBuy = [
-      { name: '사이다', quantity: 1 },
-      { name: '비타민워터', quantity: 1 },
+      { name: '사이다', quantity: 1, promotion: '탄산2+1' },
+      { name: '비타민워터', quantity: 1, promotion: 'null' },
     ];
 
     const controller = new Controller(inventory, promotions);
 
-    expect(controller.getPromotionProducts(productToBuy)).toEqual(result);
+    expect(controller.getApplicablePromotionProducts(productToBuy)).toEqual(
+      result,
+    );
   });
 
   test('프로모션 가능한 것들중 더 가져와야할 개수가 있다는 안내후 추가한다면 구매할 물품 개수를 추가한다.', async () => {
     const products = [
-      { name: '사이다', quantity: 3, promotion: '탄산2+1' },
+      { name: '사이다', quantity: 2, promotion: '탄산2+1' },
       {
         name: '탄산수',
         quantity: 1,
@@ -78,7 +80,7 @@ describe('컨트롤러 테스트', () => {
       },
     ];
 
-    mockQuestions(['[사이다-3],[탄산수-1]', 'Y', 'N']);
+    mockQuestions(['[사이다-2],[탄산수-1]', 'Y', 'N']);
 
     const controller = new Controller(inventory, promotions);
     await controller.setProducts();
@@ -87,7 +89,19 @@ describe('컨트롤러 테스트', () => {
     expect(controller.promotionProducts).toEqual([
       {
         name: '사이다',
-        quantity: 4,
+        quantity: 1,
+        promotion: '탄산2+1',
+      },
+      {
+        name: '탄산수',
+        quantity: 0,
+        promotion: '탄산2+1',
+      },
+    ]);
+    expect(controller.productsToBuy).toEqual([
+      {
+        name: '사이다',
+        quantity: 3,
         promotion: '탄산2+1',
       },
       {
@@ -98,10 +112,10 @@ describe('컨트롤러 테스트', () => {
     ]);
   });
 
-  test('프로모션 가능 재고가 있는지 체크하고, 재고 부족이면 일부 수량에 대해 정가로 결제할지 여부 안내', async () => {
+  test('프로모션 가능 재고가 있는지 체크하고, 재고 부족이면 일부 수량에 대해 정가로 결제할지 여부 안내후 정가로 결제하지 않는다면 정가로 결제해야하는 수량만큼 뺀다.', async () => {
     const products = [{ name: '사이다', quantity: 12, promotion: '탄산2+1' }];
 
-    mockQuestions(['[사이다-10]', 'N']);
+    mockQuestions(['[사이다-12]', 'N']);
 
     const controller = new Controller(inventory, promotions);
     await controller.setProducts();
@@ -110,7 +124,14 @@ describe('컨트롤러 테스트', () => {
     expect(controller.promotionProducts).toEqual([
       {
         name: '사이다',
-        quantity: 8,
+        quantity: 3,
+        promotion: '탄산2+1',
+      },
+    ]);
+    expect(controller.productsToBuy).toEqual([
+      {
+        name: '사이다',
+        quantity: 9,
         promotion: '탄산2+1',
       },
     ]);
