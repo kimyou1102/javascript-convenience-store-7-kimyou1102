0f31b321b666c584244273b4909851a54fe75f1d
kimyou1102 <yoosk5485@naver.com>
2024-11-10 04:35:37 +0900

    feat: 구매할 상품 목록과 프로모션 상품목록을 클래스의 속성에 할당
    - 프로모션으로 제공될 상품 수를 계산해서 할당
    - 각 목록에 promotion 이름을 추가적으로 붙여서 할당

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -2,6 +2,7 @@ import InputView from '../view/InputView.js';
 import OutputView from '../view/OutputView.js';
 import { INPUT_MESSAGE } from '../constants/constants.js';
 import PromotionInfo from '../model/PromotionInfo.js';
+import { parseProducts } from '../utils/parseProduct.js';
 import { DateTimes } from '@woowacourse/mission-utils';
 import InventoryManagement from '../model/InventoryManagement.js';
 
@@ -17,13 +18,40 @@ export default class Controller {
 
   async run() {
     this.outputView.printGreetingAndInventory(this.inventoryManagement.getInventoryInfo());
+    await this.setProducts();
+  }
+
+  async setProducts() {
     const productToBuyString = await this.getProductToBuy();
+    const productsToBuy = parseProducts(productToBuyString);
+    this.productsToBuy = this.getProductsWithPromotion(productsToBuy);
+    const applicablePromotionProducts = this.getApplicablePromotionProducts(this.productsToBuy);
+
+    this.setPromotionProducts(applicablePromotionProducts);
+  }
+
+  setPromotionProducts(applicablePromotionProducts) {
+    this.promotionProducts = applicablePromotionProducts.map((product) => {
+      const { buy, get } = this.promotionInfo.getPromotion(product.promotion);
+      const count = Math.floor(product.quantity / (buy + get));
+      return {
+        ...product,
+        quantity: count,
+      };
+    });
   }
 
   getApplicablePromotionProducts(productsToBuy) {
     return productsToBuy.filter((product) =>
       this.promotionInfo.isPromotionApplicable(product.promotion, DateTimes.now()),
     );
+  }
+
+  getProductsWithPromotion(productsToBuy) {
+    return productsToBuy.map((product) => {
+      const promotion = this.inventoryManagement.getPromotionNameByProductName(product.name);
+      return { ...product, promotion };
+    });
   }
 
   async getProductToBuy() {
