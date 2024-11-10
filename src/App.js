e1bcf5793e104a3e2393b5c380e867a4f2756855
kimyou1102 <yoosk5485@naver.com>
2024-11-10 18:36:08 +0900

    feat: 애플리케이션 실행 진입점 추가
    - Controller를 생성하고 run 메서드를 호출하여 프로그램이 실행되도록 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Controller from './controller/Controller.js';
+import { getProductsData, getPromotionsData } from './utils/getfileData.js';
+
 class App {
-  async run() {}
+  async run() {
+    const inventory = await getProductsData();
+    const promotions = await getPromotionsData();
+
+    const controller = new Controller(inventory, promotions);
+    controller.run();
+  }
 }
 
 export default App;
