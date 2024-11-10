8e93a7fd043b253a552da4fd02cb6a3751d76ccd
kimyou1102 <yoosk5485@naver.com>
2024-11-11 05:07:29 +0900

    refactor: 입력 안내문 상수화

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -180,29 +180,26 @@ export default class Controller {
   }
 
   async getAnswerToAddPurchase() {
-    return await this.getValidatedInputWithRetry(
-      '감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)',
-      validateAnswerInput,
-    );
+    return await this.getValidatedInputWithRetry(INPUT_MESSAGE.ADD_PURCHASE, validateAnswerInput);
   }
 
   async getAnswerToMembership() {
     return await this.getValidatedInputWithRetry(
-      '멤버십 할인을 받으시겠습니까? (Y/N)',
+      INPUT_MESSAGE.MEMBERSHIP_DISCOUNT,
       validateAnswerInput,
     );
   }
 
   async getAnswerToBuy(name, count) {
     return await this.getValidatedInputWithRetry(
-      `현재 ${name} ${count}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)`,
+      `현재 ${name} ${count}${INPUT_MESSAGE.PROMOTION_NOT_APPLICABLE}`,
       validateAnswerInput,
     );
   }
 
   async getAnswerToAddition(name, count) {
     return await this.getValidatedInputWithRetry(
-      `현재 ${name}은(는) ${count}개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)`,
+      `현재 ${name}은(는) ${count}${INPUT_MESSAGE.PROMPT_FREE_ADDITION}`,
       validateAnswerInput,
     );
   }
